6f5a2f39fad01c2b2962f69678455643b1ab6d0d
Edgar Hipp <hipp.edg@gmail.com>
2015-03-23 16:37:02 +0100

    update for docxtemplater v1.0.0

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 var mime  = require('mime'),
     Docx  = require('docxtemplater'),
+    fs = require('fs'),
     express= require('express');
 
-var app=express()
+var app=express();
 
 var index = function(req, res) {
   var file = __dirname + '/tagExample.docx';
 
   res.setHeader('Content-disposition', 'attachment; filename=test.docx');
-  
-  new Docx().loadFromFile( file, { async: true } ).success(function(docx) {
-    docx.setTags({
-     "first_name"  : "Hipp",
-     "last_name"   : "Edgar",
-     "phone"       : "0652455478",
-     "description" : "New Website"
-    });
 
-    docx.applyTags();
-    out=docx.output({download:false,type:"string"})
-    res.send(new Buffer(out,"binary"));
-    res.end();
+  var content = fs.readFileSync(file);
+
+  docx = new Docx(content);
+
+  docx.setData({
+   "first_name"  : "Hipp",
+   "last_name"   : "Edgar",
+   "phone"       : "0652455478",
+   "description" : "New Website"
   });
+
+  docx.render();
+  buf=docx.getZip()
+    .generate({type:"nodebuffer"});
+  res.send(buf);
+  res.end();
 };
 
 app.get('/',index);
